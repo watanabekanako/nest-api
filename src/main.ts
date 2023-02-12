dd6c9673ed61147ec36eefd5a059954cf6868c85
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2023-02-12 21:57:48 +0900

    csrf動作確認完了

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,19 @@ async function bootstrap() {
     origin: ['http://localhost:3000'],
   });
   app.use(cookieParser());
-  await app.listen(3005);
+  app.use(
+    csurf({
+      cookie: {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+      },
+      value: (req: Request) => {
+        return req.header('csrf-token');
+      },
+    }),
+  );
+
+  await app.listen(process.env.PORT || 3005);
 }
 bootstrap();
